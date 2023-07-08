441bf55259f4b7b6b2e4f1e1a0a1b552617c7f5d
lanjudev16 <lanjudev16@gmail.com>
2023-07-09 01:51:40 +0600

    Setup done

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Shared/Header/Header';
+import Footer from '../Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+        </div>
+    );
+};
+
+export default Main;
